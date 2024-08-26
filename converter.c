18fa6c32bc9485ed85366e9cb4e7fd1801829429
James F. <jamesfoo05@gmail.com>
2024-08-26 15:44:08 +0800

    feat: Added the conversion logic to 7 degrees of brightness

--- converter.c
+++ converter.c
@@ -4,14 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// int checkBrightnessLevel(char r, char g, char b);
-
 int main() {
     int width, height, channels;
-    // FILE* output;
+    FILE* output;
 
-    unsigned char *img = stbi_load("./images/MonaLisa.jpg", &width, &height, &channels, 0);
-    // output = fopen("OUTPUT.txt", "w");
+    unsigned char *img = stbi_load("./images/lena.jpg", &width, &height, &channels, 0);
+    output = fopen("OUTPUT.txt", "w");
 
     if (img == NULL) {
         printf("Error: Image Failed to Load.\n");
@@ -20,27 +18,39 @@ int main() {
         printf("Image loaded: %dx%d with %d channels\n", width, height, channels);
     }
 
-    /*
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int pixel_index = (y * width + x) * channels;
-                unsigned char r = img[pixel_index];
-                unsigned char g = img[pixel_index + 1];
-                unsigned char b = img[pixel_index + 2];
 
-                // int brightness = checkBrightnessLevel(r, g, b);
-                
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            int pixel_index = (y * width + x) * channels;
+            unsigned char r = img[pixel_index];
+            unsigned char g = img[pixel_index + 1];
+            unsigned char b = img[pixel_index + 2];
+
+            float brightness = ((r + g + b) / 3.0) / 255.0;
+
+            if (brightness < .20)  {
+                fprintf(output, " ");
+            } else if (brightness >= .20 && brightness < .30) {
+                fprintf(output, ".");
+            } else if (brightness >= .30 && brightness < .40) {
+                fprintf(output, ":");
+            } else if (brightness >= .40 && brightness < .50) {
+                fprintf(output, "-");
+            } else if (brightness >= .50 && brightness < .60) {
+                fprintf(output, "=");
+            } else if (brightness >= .60 && brightness < .80) {
+                fprintf(output, "#");
+            } else if (brightness >= .80) {
+                fprintf(output, "@");
             }
+            
         }
-    */
+        fprintf(output, "\n");
+    }
+
+    printf("File Converted\n");
 
     stbi_image_free(img);
-    // fclose(img);
+    fclose(output);
     return 0;
 }
-
-/*
-    int checkBrightnessLevel(char r, char g, char b) {
-    return BrightnessLevel;
-}
-*/
